7085653b8740aef6f43f533665d168809ec9fe15
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-02-01 11:40:12 -0300

    update http status on pacientes and create method detalhar

--- src/main/java/med/voll/api/controllers/PacienteController.java
+++ src/main/java/med/voll/api/controllers/PacienteController.java
@@ -2,17 +2,16 @@ package med.voll.api.controllers;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.pacientes.DadosAtualizacaoPaciente;
-import med.voll.api.pacientes.DadosCadastroPaciente;
-import med.voll.api.pacientes.Paciente;
-import med.voll.api.pacientes.PacienteRepository;
+import med.voll.api.pacientes.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController()
@@ -23,17 +22,20 @@ public class PacienteController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<Paciente> cadastrar(@RequestBody @Valid DadosCadastroPaciente dados) {
+    public ResponseEntity<Paciente> cadastrar(@RequestBody @Valid DadosCadastroPaciente dados, UriComponentsBuilder uriBuilder) {
         Paciente paciente = repository.save(new Paciente(dados));
-        return ResponseEntity.ok().body(paciente);
+        URI uri = uriBuilder.path("/pacientes/{id}")
+                .buildAndExpand(paciente.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(paciente);
     }
 
     @PutMapping("/{id}")
     @Transactional
-    public ResponseEntity<Void> atualizar(@PathVariable Long id, @RequestBody @Valid DadosAtualizacaoPaciente dados) {
+    public ResponseEntity<DadosDetalhamentoPaciente> atualizar(@PathVariable Long id, @RequestBody @Valid DadosAtualizacaoPaciente dados) {
         Paciente paciente = repository.getReferenceById(id);
         paciente.atualizar(dados);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(new DadosDetalhamentoPaciente(paciente));
     }
 
     @GetMapping
@@ -48,5 +50,11 @@ public class PacienteController {
         Paciente paciente = repository.getReferenceById(id);
         paciente.excluir();
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DadosDetalhamentoPaciente> detalhar(@PathVariable Long id) {
+        Paciente paciente = repository.getReferenceById(id);
+        return ResponseEntity.ok().body(new DadosDetalhamentoPaciente(paciente));
     }
 }
